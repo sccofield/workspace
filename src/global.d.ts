cc574564d5dbbbbd98b2c0087f6a888680ce31c3
sccofield <michael_umanah@yahoo.com>
2025-01-09 07:01:48 +0100

    disable type che k for paystackpop

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,6 @@
 declare global {
   interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     PaystackPop: any; // You can use a more specific type if Paystack provides one
   }
 }
