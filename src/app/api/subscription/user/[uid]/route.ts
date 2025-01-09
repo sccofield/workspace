f312efe5dbf5fd1eeefdad4e11ada2b2b7ba67b4
sccofield <michael_umanah@yahoo.com>
2025-01-09 06:48:05 +0100

    fix Get error in subscription api

--- src/app/api/subscription/user/[uid]/route.ts
+++ src/app/api/subscription/user/[uid]/route.ts
@@ -1,8 +1,11 @@
 import { NextResponse } from 'next/server';
 import { supabase } from '@/utils/supabase';
 
-export async function GET(req: Request, context: { params: { uid: string } }) {
-  const { uid } = context.params; // Explicitly typed
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ uid: string }> }
+) {
+  const { uid } = await params; // Explicitly typed
 
   try {
     const { data, error } = await supabase
